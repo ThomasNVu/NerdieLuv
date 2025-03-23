8d5b1cba87d6e25c39232228aeaedd94ba9b6344
ThomasNVU <thomasvu.tech@gmail.com>
2025-03-23 19:10:36 -0400

    Revise HTML to spec sheet

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
-<?php session_start(); /* Starts the session */
+<?php
+include 'common.php';
 
-if(!isset($_SESSION['UserData']['Username'])){
-	header("location:login.php");
-	exit;
-}
+// Display the header with a custom title
+displayHeader("Home Page");
 ?>
 
-Congratulation! You have logged into password protected page. <a href="logout.php">Click here</a> to Logout.
+<a href="./signup.php">
+	<img src="./images/notepad.png"height="30px" style="vertical-align: middle;"> 
+	<p style="display: inline; margin: 0;">Sign up for a new account</p>
+</a>
+<br>
+<a href="./matches.php">
+	<img src="./images/heart.png"height="30px" style="vertical-align: middle;"> 
+	<p style="display: inline; margin: 0;">Check your matches</p>
+</a>
+
+<p>This page is for single people to meet and date each other! Type in <br>
+your personal information and wait for the dating site to match you up!<br>
+Thank you for using our site.</p>
+
+<?php
+// Display the footer
+displayFooter();
+?>
